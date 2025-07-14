7a71013e32dec8f3e73fac561d06220829b4c13c
louisa :3 <louisakavindu@gmail.com>
2025-07-14 09:08:53 -0400

    will this work........

---
+++ google_ai_mode_remover.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Bye bye Google AI Mode :P
+// @namespace    https://github.com/v1peridae/remove-ai-mode-from-google
+// @version      1.0
+// @description  Me when the function has AI Mode (not cool)
+// @author       v1peridae
+// ==/UserScript==
+
+(() => {
+  "use strict";
+  const selector = 'div[jsname="bVqjv"].YmvwI';
+  function removeAI() {
+    document.querySelectorAll(selector).forEach((el) => el.remove());
+    document.querySelectorAll("span.Beswgc").forEach((span) => {
+      if (span.textContent.trim() === "AI Mode") {
+        const cont = span.closest("div");
+        if (cont) {
+          cont.remove();
+        }
+      }
+    });
+  }
+  removeAI();
+  document.addEventListener("DOMContentLoaded", removeAI);
+  const observer = new MutationObserver(removeAI);
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+})();
